9d1a29cc55100fd971b992bceb46ca1fd80e9c75
David Morgan <dmorgan81@gmail.com>
2012-08-17 12:27:11 -0400

    Create annotated tags upon release
    Lightweight tags aren't as good.

--- chrome-release.sh
+++ chrome-release.sh
@@ -2,4 +2,4 @@
 
 version=$(cat manifest.json | awk '/"version"/ { gsub(/[",]/,""); print $2 }')
 zip -r scumdar-$version.zip . -x chrome-release.sh .git/\* README.md
-git tag scumdar-$version
+git tag -a v$version -m "version $version"
