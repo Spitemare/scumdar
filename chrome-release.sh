679a515dc55690abd4941a53e5d8946def1c57c0
David Morgan <dmorgan81@gmail.com>
2013-03-28 10:55:26 -0400

    Expand release script
    Minimize CSS and JavaScript on release. Tag only if not doing a test
    build.

--- chrome-release.sh
+++ chrome-release.sh
@@ -1,5 +1,18 @@
-#!/bin/sh
+#!/bin/bash
+
+CWD=$(pwd)
+
+mkdir package/
+cp -r css/ img/ js/ manifest.json package/
+cd package/
+shopt -s extglob
+for file in js/!(*.min).js css/!(*.min).css; do
+    yuicompressor $file -o $file
+done
+shopt -u extglob
 
 version=$(cat manifest.json | awk '/"version"/ { gsub(/[",]/,""); print $2 }')
-zip -r scumdar-$version.zip . -x chrome-release.sh .git/\* README.md -x *.zip
-git tag -a v$version -m "version $version"
+zip -r $CWD/scumdar-$version.zip .
+cd $CWD
+rm -rf package/
+[[ $1 != 'test' ]] && git tag -a v$version -m "version $version"
